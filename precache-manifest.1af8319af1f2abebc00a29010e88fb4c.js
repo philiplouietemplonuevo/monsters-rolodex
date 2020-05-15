4803b9d341c19d8e58a0dd9aea3a3eb924215a17
Philip Templonuevo <philiplouietemplonuevo@gmail.com>
2020-05-15 18:15:19 -0400

    Updates

---
+++ precache-manifest.1af8319af1f2abebc00a29010e88fb4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198ac943933c102caab09fc49f3ec86e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b867d1bb71b1db3e81a1",
+    "url": "/monsters-rolodex/static/css/main.c9b9857f.chunk.css"
+  },
+  {
+    "revision": "cbf4813ca50c4bf1c0f5",
+    "url": "/monsters-rolodex/static/js/2.abae253f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.abae253f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b867d1bb71b1db3e81a1",
+    "url": "/monsters-rolodex/static/js/main.abc98abe.chunk.js"
+  },
+  {
+    "revision": "35ae7ec646daef450ee1",
+    "url": "/monsters-rolodex/static/js/runtime-main.497c0aec.js"
+  }
+]);
